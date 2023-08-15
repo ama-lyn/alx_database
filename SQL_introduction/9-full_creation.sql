2d84cc4d277a6e420686f242638197e74c50aa0a
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-15 18:20:33 +0300

    Add SQL task

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -6,12 +6,18 @@
 -- Creates few records:
 
 
-CREAT TABLE IF NOT EXISTS second_table (
+-- Create the table if it doesn't exist
+CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
-INSERT INTO second_table VALUES(1,"John", 10);
-INSERT INTO second_table VALUES(2, "Alex", 3);
-INSERT INTO second_table VALUES(3, "Bob", 14);
-INSERT INTO second_table VALUES(4, "George", 8);
+
+-- Insert records into the table
+INSERT INTO second_table VALUES (1, "John", 10);
+INSERT INTO second_table VALUES (2, "Alex", 3);
+INSERT INTO second_table VALUES (3, "Bob", 14);
+INSERT INTO second_table VALUES (4, "George", 8);
+
+-- Display records from the table
+SELECT * FROM second_table;
