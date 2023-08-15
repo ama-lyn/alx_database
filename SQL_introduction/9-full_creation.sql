8f560a9ef552b7261ffd7c5f6229ed9f4af2c4ca
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-15 18:11:54 +0300

    Add SQL task

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -11,7 +11,7 @@ CREAT TABLE IF NOT EXISTS second_table (
     name VARCHAR(256),
     score INT
 );
-INSERT INTO second_table VALUES(1,"John", 10)
-INSERT INTO second_table VALUES(2, "Alex", 3)
-INSERT INTO second_table VALUES(3, "Bob", 14)
-INSERT INTO second_table VALUES(4, "George", 8)
+INSERT INTO second_table VALUES(1,"John", 10);
+INSERT INTO second_table VALUES(2, "Alex", 3);
+INSERT INTO second_table VALUES(3, "Bob", 14);
+INSERT INTO second_table VALUES(4, "George", 8);
