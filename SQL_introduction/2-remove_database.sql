bbdac50c76a25526eb4c86b5530dc6f81d114346
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-15 18:01:34 +0300

    Delete a Database

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,5 +1,5 @@
 -- Delete an entire database 'hbtn_0c_0'
 -- If the database hbtn_0c_0 doesn’t exist, the script should not fail
--- Not allowed to use the SELECT or SHOW statements
+-- Not allowed to use the SELECT or SHOW statements.
 
 DROP DATABASE IF EXISTS hbtn_0c_0;
