f81104e9fa7500e4fe231a6d522967081f5d80d2
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-15 06:23:01 +0300

    Create Database

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- Create a database 'hbtn_0c_0' 
+-- if the database already exists, the script shouldn't fail
+-- not allowed to use the SELECT or SHOW statements
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
