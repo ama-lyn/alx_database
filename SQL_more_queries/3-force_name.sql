42d16ac91536a525855b0fbf46105e028cf1cec9
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-16 11:51:37 +0300

    Create Table

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -5,7 +5,7 @@
 -- The database name will be passed as an argument of the mysql command
 -- If the table force_name already exists, your script should not fail
 
-CREATE TABLE IF NOT EXISTS force_name VALUES(
+CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
 );
