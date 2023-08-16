67fe816cac5f3894c8a4944883fd1a7dfc8f9980
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-08-16 12:27:44 +0300

    SQL TASK

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,7 +4,6 @@
 -- You are not allowed to use the JOIN keyword
 -- The database name will be passed as an argument of the mysql command
 
-USE hbtn_0d_usa;
 SELECT id, name
 FROM cities
 WHERE state_id = (
